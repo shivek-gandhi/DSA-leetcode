1824c7ed6ee5ecd55106467122e281deadc23659
Shivek Gandhi <70126492+shivek-gandhi@users.noreply.github.com>
2025-07-04 00:19:13 +0530

    Time: 52 ms (5.82%), Space: 65.4 MB (5.5%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-    char getChar(int k, int depth) {
-    if (k == 1)
-        return 'a';
-    
-    int mid = 1 << (depth - 1);
-    
-    if (k <= mid)
-        return getChar(k, depth - 1);
-    else {
-        char prevChar = getChar(k - mid, depth - 1);
-        
-        return (prevChar == 'z') ? 'a' : (prevChar + 1);
-    }
-}
     char kthCharacter(int k) {
-        int depth = 0;
-    while ((1 << depth) < k)
-        depth++;
-    
-    return getChar(k, depth);
+        string s = "a";
+        while(s.size()<k){
+            int n = s.length();
+            for(int i=0;i<n;i++){
+                char next = 'a'+((s[i]-'a')+1) %26;
+                s= s+ next; 
+            }
+        }
+        return s[k-1];
+
     }
 };
