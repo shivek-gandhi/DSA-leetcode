5f95f24642be6cf6b82f9cc2218876057a3fbef6
Shivek Gandhi <70126492+shivek-gandhi@users.noreply.github.com>
2026-01-02 21:13:53 +0530

    Time: 3 ms (47.45%), Space: 33.5 MB (38.33%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        vector<int> freq(10001,0);
+        for(int i=0;i< nums.size();i++){
+            freq[nums[i]]++;
+            if(freq[nums[i]]>1){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
