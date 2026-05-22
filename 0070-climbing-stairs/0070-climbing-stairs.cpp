8df7a85f5fbdbb0d2daf1af97a2a437c97b09fc4
Shivek Gandhi <70126492+shivek-gandhi@users.noreply.github.com>
2026-05-23 00:04:12 +0530

    Time: 3 ms (0.39%), Space: 8.5 MB (46.4%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int f(int n, vector<int>&dp){
+        if(n<=2) return n;
+        if(dp[n] != -1) dp[n];
+        return dp[n] = f(n-1,dp) + f(n-2,dp);
+    }
+    int climbStairs(int n) {
+       if(n<=2) return n;
+       vector<int> dp(n+1);
+       dp[0]=0;
+       dp[1]=1;
+       dp[2]=2;
+       for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+       }
+       return dp[n];
+    }
+};
