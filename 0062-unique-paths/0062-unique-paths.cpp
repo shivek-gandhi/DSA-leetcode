6f3820de1a8432d8fc565febd26414852c25f9dc
Shivek Gandhi <70126492+shivek-gandhi@users.noreply.github.com>
2026-05-25 01:20:43 +0530

    Time: 0 ms (100%), Space: 9.5 MB (28.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -11,6 +11,20 @@ public:
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,dp);
+        dp[0][0] = 1;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                int down=0;
+                int right = 0;
+                if(i>0) down = dp[i-1][j];
+                if(j>0) right = dp[i][j-1];
+
+                dp[i][j] = down + right;
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
