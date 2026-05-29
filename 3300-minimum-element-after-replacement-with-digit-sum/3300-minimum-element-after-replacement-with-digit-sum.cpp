f8f2070419b2059dbea2d537fcfa667c9307fddb
Shivek Gandhi <70126492+shivek-gandhi@users.noreply.github.com>
2026-05-29 17:41:06 +0530

    Time: 0 ms (100%), Space: 27.6 MB (96.6%) - LeetHub

---
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int a=0;
+        for(int i=0;i<nums.size();i++){
+            int num = nums[i];
+            // a = a+ num %10;
+            // num = num /10;
+            while(num != 0){
+                a += num %10;
+                num = num/10;
+            }
+            nums[i] = a;
+            a = 0;
+        }
+        int mini = INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            mini = min(mini,nums[i]);
+        }
+        return mini;
+    }
+};
