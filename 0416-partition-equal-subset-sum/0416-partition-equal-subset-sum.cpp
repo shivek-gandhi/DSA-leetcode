31b9f3556681be06be5e6857ac376f04ecab4737
Shivek Gandhi <70126492+shivek-gandhi@users.noreply.github.com>
2026-06-11 23:07:17 +0530

    Time: 107 ms (66.76%), Space: 110.6 MB (30.73%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -23,8 +23,26 @@ public:
         if(sum % 2 ==1) return false;
         else{
             int k = sum/2;
-            vector<vector<int>>dp(n,vector<int>(k+1,-1));
-            return solve(n-1,k,nums,dp);
+            vector<vector<int>>dp(n,vector<int>(k+1,0));
+            // return solve(n-1,k,nums,dp);
+             for(int i=0;i<n;i++){
+                dp[i][0] = true;
+             }
+             if(nums[0]<= k){
+                dp[0][nums[0]]=true;
+             }
+             for(int ind = 1;ind<n;ind++){
+                for(int target = 1;target<=k;target++){
+                    bool notTaken = dp[ind - 1][target];
+                    bool taken = false;
+                    if (nums[ind] <= target) {
+                        taken = dp[ind - 1][target - nums[ind]];
+                    }
+                    dp[ind][target] = notTaken || taken;
+                }
+             }
+             return dp[n-1][k];
         }
+        
     }
 };
