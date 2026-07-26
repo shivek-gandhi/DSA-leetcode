c80b380f76b87532b99c5feddda194babbfaf173
Shivek Gandhi <70126492+shivek-gandhi@users.noreply.github.com>
2026-07-26 15:37:06 +0530

    Time: 16 ms (18.84%), Space: 31.5 MB (76.45%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -5,7 +5,8 @@ public:
         int n = nums.size();
         int prod1 = nums[n-1]*nums[n-2]* nums[n-3];
         int prod2 = nums[0]*nums[1] * nums[n-1];
-        if(prod1 > prod2) return prod1;
-        else return prod2;
+        // if(prod1 > prod2) return prod1;
+        // else return prod2;
+        return max(prod1,prod2);
     }
 };
