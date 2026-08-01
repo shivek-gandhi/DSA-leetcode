d46fe74ef4271a32eea7a8028f89bbd08f754530
Shivek Gandhi <70126492+shivek-gandhi@users.noreply.github.com>
2026-08-02 00:29:36 +0530

    Time: 3 ms (22.16%), Space: 9.5 MB (60.57%) - LeetHub

--- 4006-count-valid-prefixes/4006-count-valid-prefixes.cpp
+++ 4006-count-valid-prefixes/4006-count-valid-prefixes.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int countValidPrefixes(string s) {
-        int result = 0;
-        int diff = 0;
-        for(auto c: s){
-            diff += c=='1' ? 1 : -1;
-            result += abs(diff) <= 1;
+        int ans = 0;
+        int count0 = 0;
+        int count1 = 0;
+        for(char c : s){
+            if(c == '0') count0++;
+            else count1++;
+            if (abs(count1 - count0)<= 1) ans++;
         }
-        return result;
+        return ans;
     }
 };
