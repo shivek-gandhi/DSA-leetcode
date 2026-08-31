6314f32962978b02370994eaf71d44bdc621b401
Shivek Gandhi <70126492+shivek-gandhi@users.noreply.github.com>
2026-08-31 09:17:23 +0530

    Time: 0 ms (100%), Space: 124.6 MB (60.57%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        vector<int> result = {-1, -1};
+
+        int minDistance = INT_MAX;
+
+        ListNode* previousNode = head;
+        ListNode* currentNode = head->next;
+        int currentIndex = 1;
+        int previousCriticalIndex = 0;
+        int firstCriticalIndex = 0;
+
+        while (currentNode->next != nullptr) {
+            // Check if the current node is a local maxima or minima
+            if ((currentNode->val < previousNode->val &&
+                 currentNode->val < currentNode->next->val) ||
+                (currentNode->val > previousNode->val &&
+                 currentNode->val > currentNode->next->val)) {
+                // If this is the first critical point found
+                if (previousCriticalIndex == 0) {
+                    previousCriticalIndex = currentIndex;
+                    firstCriticalIndex = currentIndex;
+                } else {
+                    // Calculate the minimum distance between critical points
+                    minDistance =
+                        min(minDistance, currentIndex - previousCriticalIndex);
+                    previousCriticalIndex = currentIndex;
+                }
+            }
+
+            // Move to the next node and update indices
+            currentIndex++;
+            previousNode = currentNode;
+            currentNode = currentNode->next;
+        }
+
+        // If at least two critical points were found
+        if (minDistance != INT_MAX) {
+            int maxDistance = previousCriticalIndex - firstCriticalIndex;
+            result = {minDistance, maxDistance};
+        }
+
+        return result;
+    }
+};
