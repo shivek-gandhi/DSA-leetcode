2d6cc91e520ddf55d471a56aa783ed62017e5593
Shivek Gandhi <70126492+shivek-gandhi@users.noreply.github.com>
2026-09-15 16:41:40 +0530

    Time: 411 ms (14.66%), Space: 253.8 MB (5.76%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -28,7 +28,7 @@ public:
         dp2.resize(n+1,-1);
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+            for(int j=i;j<n;j++){
                 solve(i,j,s);
             }
         }
